b21452d687f35d8f14f563e7d2d0f9ef13897328
Adam Budai <budai8adam@gmail.com>
2018-08-09 17:04:19 +0200

    Finished code for linear regression. And almost finished for jupyter notebook.

--- bias-variance/linearregr.py
+++ bias-variance/linearregr.py
@@ -3,15 +3,15 @@ from matplotlib.pyplot import plot, show
 
 # An implementation for the linear regression with gradient descent.
 
-def data_generator(n, a, b, x_min, x_max, sigma=0.01):
+def data_generator(n, fn, x_min=0.0, x_max=1.0, sigma=0.01):
     delta_x = (x_max - x_min) / (n - 1) # in case of n points the number of intervals are n-1
     
     x = np.array([x_min + t * delta_x for t in range(n)], dtype=float)
-    y = a* x + b
+    y = fn(x)
     
     mean = [0, 0]
     cov = [[sigma, 0], [0, sigma]]
-    x_noise, y_noise = np.random.multivariate_normal(mean, cov, n)
+    x_noise, y_noise= np.random.multivariate_normal(mean, cov, n).T
 
     return x + x_noise, y + y_noise
 
@@ -26,7 +26,7 @@ def loss(theta, train_x, train_y):
     X = np.dot(train_x, theta)
     X = X - train_y
     X = np.square(X)
-    return np.sum(X)
+    return np.sum(X)/X.shape[0]
 
 
 # Calculate gradient
@@ -34,7 +34,7 @@ def gradient(theta, train_x, train_y):
     grad = np.dot(train_x, theta)
     grad = grad - train_y
     grad = np.dot(grad, train_x)
-    return grad
+    return grad/np.linalg.norm(grad)
 
 
 # Update the weights
@@ -50,19 +50,20 @@ def linear_regressor(data, lr=0.1, max_iter=100, split_ratio=0.8, verbose=False)
     split_ratio - the ratio of the training
     verbose - to show the loss during training
     '''
+    x, y = data[0], data[1]
 
     # Split the data randomly
-    DATA_LEN = len(data)
+    DATA_LEN = len(data[0])
     indices = np.array([x for x in range(DATA_LEN)], dtype=int)
     np.random.shuffle(indices)
-    training_indices = indices[0:int(DATA_LEN*split_ratio)]
-    test_indices = indices[int(DATA_LEN*split_ratio):-1]
+    training_indices = indices[0:int(DATA_LEN*split_ratio)].tolist()
+    test_indices = indices[int(DATA_LEN*split_ratio):-1].tolist()
 
-    train_x = np.array([data[training_indices[i]][0] for i in len(training_indices)])
-    train_y = np.array([data[training_indices[i]][1] for i in len(training_indices)])
+    train_x = np.array([x[i] for i in training_indices])
+    train_y = np.array([y[i] for i in training_indices])
 
-    test_x = np.array([data[test_indices[i]][0] for i in len(test_indices)])
-    test_y = np.array([data[test_indices[i]][1] for i in len(test_indices)])
+    test_x = np.array([x[i] for i in test_indices])
+    test_y = np.array([y[i] for i in test_indices])
 
     # Insert 1 at the end
     temp_x = np.ones((len(train_x), 2))
@@ -85,6 +86,23 @@ def linear_regressor(data, lr=0.1, max_iter=100, split_ratio=0.8, verbose=False)
         theta = update(theta, grad, lr)
         train_losses.append((itr, train_L))
         test_losses.append((itr, test_L))
+        if verbose and (itr + 1) % (max_iter // 10) == 0:
+            print("Iterating: [%d%%]\r" %int(100*(itr + 1)/max_iter), end="")
+    if verbose:
+        print("")
     
     # Return: a, b, train_loss list(iter, loss), test_loss (iter, loss)
     return theta[0], theta[1], train_losses, test_losses
+
+
+# Calculating the bias, variance and noise
+def bias_var(ys_pred, y):
+    '''
+    ys_pred - numpy array containing the predictions with each function
+    y - measured y in x
+    '''
+    bias = (np.average(ys_pred) - y)**2
+    var = np.var(ys_pred)
+    return bias, var
+
+
--- bias-variance/test.py
+++ bias-variance/test.py
@@ -1,5 +1,6 @@
 import linearregr as L
 import numpy as np
+from matplotlib import pyplot as plt
 
 def test_loss():
     theta = np.array([1.0, 0.0])
@@ -10,6 +11,7 @@ def test_loss():
     for i in range(y.shape[0]):
         y_ = theta[0] * x[i][0] + theta[1]
         loss_1 += (y_ - y[i])**2
+    loss_1 = loss_1 / y.shape[0]
     
     assert abs(loss_0 - loss_1) < 0.001, "Wrong loss value!"
     print("Loss: Correct!")
@@ -25,9 +27,27 @@ def test_gradient():
     for i in range(y.shape[0]):
         y_ = theta[0] * x[i][0] + theta[1]
         grad_1 += (y_ - y[i]) * x[i]
+    grad_1 /= np.linalg.norm(grad_1)
     
     assert np.allclose(grad_0, grad_1), "Wrong gradient value!"
     print("Gradient: Correct!")
 
 test_gradient()
 
+def test_generator():
+    x, y = L.data_generator(30, lambda x: 1.0 *x + 0.0, sigma=0.001)
+    plt.plot(x, y, 'ro')
+    plt.show()
+
+test_generator()
+
+def test_bias_var_noise():
+    y_q = 1.1
+    ys_pred = np.array([1.05, 1.0, .97, .99, 1.08])
+
+    b, v = L.bias_var(ys_pred, y_q)
+    eps = np.average(np.square(ys_pred - y_q))
+    assert abs(b+v - eps) < 0.000001, "Wrong bias-variance calculation!"
+    print("Bias-var: Correct!")
+
+test_bias_var_noise()
