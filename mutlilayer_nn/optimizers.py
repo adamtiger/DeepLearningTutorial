4eea1ee6ba070377d8b1fb37803d1ee69a1ab25b
Adam Budai <budai8adam@gmail.com>
2018-08-14 11:38:48 +0200

    Optimizer SGD is done.

--- mutlilayer_nn/optimizers.py
+++ mutlilayer_nn/optimizers.py
@@ -1,9 +1,20 @@
 
 
 class Optimizer:
-
+     
     def __init__(self, lr):
         self.lr = lr
 
     def optimizer_step(self, theta, gradients):
         pass
+
+
+class SGD(Optimizer):
+
+    def __init__(self, lr):
+        super().__init__(lr)
+    
+    def optimizer_step(self, theta, gradients):
+
+        for idx, (w, grad) in enumerate(zip(theta, gradients)):
+            theta[idx] = w + self.lr * grad
