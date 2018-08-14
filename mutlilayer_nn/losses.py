b4721e274aeb6909fa9a6f240cf9c6a29cfa525c
Adam Budai <budai8adam@gmail.com>
2018-08-14 13:57:22 +0200

    Multi layer nn - forward and backward were implemented.

--- mutlilayer_nn/losses.py
+++ mutlilayer_nn/losses.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Loss:
 
-    def loss(self, y_predicted, y):
+    def loss(self, ys_predicted, ys):
         pass
 
     def delta_last(self, y_predicted, y):
@@ -11,32 +11,30 @@ class Loss:
 
 class MeanSquaredError(Loss):
 
-    def loss(self, y_predicted, y):
+    def loss(self, ys_predicted, ys):
         L = 0
-        for y_p, y_ in zip(y_predicted, y):
-            L += np.dot(y_p - y_, y_p - y_)
-        return 0.5 / len(y) * L
+        for y_p, y in zip(ys_predicted, ys):
+            L += np.dot(y_p - y, y_p - y)
+        return 0.5 / len(ys) * L
 
     def delta_last(self, y_predicted, y):
-        delta = np.zeros_like(y[0])
-        for y_p, y_ in zip(y_predicted, y):
-            delta += y_p - y_
-        return 1.0 / len(y) * delta
+        delta = np.zeros_like(y)
+        delta += y_predicted - y
+        return 1.0 * delta
 
 
 class CrossEntropy(Loss):
 
-    def loss(self, y_predicted, y):
+    def loss(self, ys_predicted, ys):
         L = 0
-        for y_p, y_ in zip(y_predicted, y):
-            L += np.dot(y_p, np.log(y_))
-        return -1.0 / len(y) * L
+        for y_p, y in zip(ys_predicted, ys):
+            L += np.dot(y_p, np.log(y))
+        return -1.0 / len(ys) * L
 
     def delta_last(self, y_predicted, y):
-        delta = np.zeros_like(y[0])
-        for y_p, y_ in zip(y_predicted, y):
-            delta += y_ / y_p
-        return -1.0 / len(y) * delta
+        delta = np.zeros_like(y)
+        delta += y / y_predicted
+        return -1.0 * delta
 
 
 class Huber(Loss):
@@ -44,25 +42,25 @@ class Huber(Loss):
     def __init__(self, delta=1.0):
         self.delta = delta
 
-    def loss(self, y_predicted, y):
+    def loss(self, ys_predicted, ys):
         L = 0.0
-        for y_p, y_ in zip(y_predicted, y):
-            diff = np.abs(y_ - y_p)
+        for y_p, y in zip(ys_predicted, ys):
+            diff = np.abs(y - y_p)
             mask = np.greater_equal(self.delta, diff)
             _mask = np.greater(diff, self.delta)
-            sqr = 0.5 * np.square(y_ - y_p)
+            sqr = 0.5 * np.square(y - y_p)
             lin = self.delta * diff - 0.5 * self.delta**2
             L += np.sum(sqr * mask + lin * _mask)
-        return L / len(y) 
+        return L / len(ys) 
 
     def delta_last(self, y_predicted, y):
-        d = np.zeros_like(y[0])
-        for y_p, y_ in zip(y_predicted, y): # solution to avoid if-else
-            abs_diff = np.abs(y_ - y_p)
-            mask = np.greater_equal(self.delta, abs_diff) # True: 1, False: 0
-            _mask = np.greater(abs_diff, self.delta)
-            diff = y_ - y_p
-            c = np.sign(y_p - y_) * self.delta
-            d += diff * mask + c * _mask
-        return d / len(y)
+        d = np.zeros_like(y)
+        # solution to avoid if-else
+        abs_diff = np.abs(y - y_predicted)
+        mask = np.greater_equal(self.delta, abs_diff) # True: 1, False: 0
+        _mask = np.greater(abs_diff, self.delta)
+        diff = y - y_predicted
+        c = np.sign(y_predicted - y) * self.delta
+        d += diff * mask + c * _mask
+        return d 
 
--- mutlilayer_nn/mlp.py
+++ mutlilayer_nn/mlp.py
@@ -1,2 +1,74 @@
 import numpy as np
+from mutlilayer_nn import activations, losses, optimizers
 
+
+class Mlp:
+    
+    def __init__(self, optimizer, loss, initializer, regularizer):
+        self.optimizer = optimizer
+        self.loss = loss
+        self.initializer = initializer
+        self.regularizer = regularizer 
+
+        self.theta = []
+        self.activations = []
+        self.gradients = []
+
+        self.layers = 0 # number of layers
+    
+    def add_layer(self, nodes, input_length=0, activation=activations.Linear()):
+        if input_length==0:
+            if len(self.theta) > 0:
+                input_length = self.theta[-1].shape[0]
+            else:
+                assert "Missing input_length at first layer!"
+
+        w = self.initializer.create((input_length, nodes))
+        self.theta.append(w)
+        self.activations.append(activation)
+        self.layers += 1
+    
+    def __forward(self, x):
+        '''
+        x - an input vector, only one sample
+        '''
+        w_times_xs = []
+        hs = []
+        x_current = x
+        for w, a in zip(self.theta, self.activations):
+            w_times_x = np.matmul(w, x_current)
+            h = a.activate(w_times_x)
+            w_times_xs.append(w_times_x)
+            hs.append(h)
+            x_current = h
+        return x_current, w_times_xs, hs
+
+    def __backward(self, w_times_xs, hs, y_real):
+        '''
+        w_times_xs - w product x for all hidden layers (list)
+        hs - outputs of each layer (list)
+        y_real - a real output from the training set (one sample)
+        '''
+        y_predicted = hs[-1]
+        delta = self.loss.delta_last(y_predicted, y_real)
+        for l in range(self.layers-1, -1, -1): # walking the list backward direction
+            wx = w_times_xs[l]
+            h = hs[l]
+            df = self.activations[l].d_activate
+            df_wx = np.matmul(df(wx), delta)
+            self.gradients[l] += np.outer(df_wx, h) # calculate gradient for delta
+            delta = np.matmul(df_wx, self.theta[l]) # calculate new delta
+
+    def __init_gradients(self):
+        if len(self.gradients) == 0:
+            for w in self.theta:
+                self.gradients.append(np.zeros_like(w))
+        else:
+            for idx in range(self.layers):
+                self.gradients[idx] *= 0.0
+    
+    def __multiply_gradient_list(self, factor):
+        self.gradients = list(map(lambda x: factor * x, self.gradients))
+
+
+    
